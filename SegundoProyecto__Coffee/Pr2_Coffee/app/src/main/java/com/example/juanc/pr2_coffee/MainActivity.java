df194450986ab01a8be15702476cd6edb84453e0
Juan-Carrasco <jca37@alumnos.unican.es>
2017-05-01 12:52:21 +0200

    Final version. Included email intent support

--- SegundoProyecto__Coffee/Pr2_Coffee/app/src/main/java/com/example/juanc/pr2_coffee/MainActivity.java
+++ SegundoProyecto__Coffee/Pr2_Coffee/app/src/main/java/com/example/juanc/pr2_coffee/MainActivity.java
@@ -7,17 +7,23 @@ package com.example.juanc.pr2_coffee;
  * package com.example.android.justjava;
  */
 
+import android.content.Intent;
 import android.icu.text.NumberFormat;
 import android.icu.text.StringPrepParseException;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.net.URI;
 
 /**
  * This app displays an order form to order coffee.
@@ -25,9 +31,10 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
     int numberOfCoffees = 0;
     static final int DEFAULT_PRICE = 5;
+    static final int MAX_COFFEE = 100;
     // Declaration of Views
 
-    TextView textview_quantity, textview_order;
+    TextView textview_quantity;// textview_order;
     EditText edittext_name;
     CheckBox checkbox_cream, checkbox_chocolate;
     String string_message;
@@ -36,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textview_quantity = (TextView) findViewById(R.id.quantity_text_view);
-        textview_order = (TextView) findViewById(R.id.text_view_result);
+        //textview_order = (TextView) findViewById(R.id.text_view_result);
         edittext_name = (EditText) findViewById(R.id.edit_text_name);
         checkbox_cream = (CheckBox) findViewById(R.id.checkbox_cream);
         checkbox_chocolate = (CheckBox) findViewById(R.id.checkbox_chocolate);
@@ -52,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
         if(checkbox_chocolate.isChecked()){
             price = price + 2;
         }
-        Log.d("DEBUG", "ENTRA A SUBMIT");
 
         displayMessage(price);
     }
@@ -64,7 +70,15 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
-
+    private void sendMail(String name, String message){
+        Intent intent_mail = new Intent(Intent.ACTION_SENDTO);
+        intent_mail.setData(Uri.parse("mailto:"));
+        intent_mail.putExtra(Intent.EXTRA_TEXT,message);
+        intent_mail.putExtra(Intent.EXTRA_SUBJECT, "ORDER FROM " + name);
+        if (intent_mail.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent_mail);
+        }
+    }
     private void displayMessage(int price){
         string_message = "";
         string_message = string_message.concat("Name: ");
@@ -78,15 +92,27 @@ public class MainActivity extends AppCompatActivity {
         string_message = string_message.concat("\nTotal: ");
         string_message = string_message.concat(String.valueOf(numberOfCoffees * price));
         string_message = string_message.concat("$");
-        textview_order.setText(string_message);
+        sendMail(edittext_name.getText().toString(),string_message);
+        Log.d("DEBUG", "1");
+        //textview_order.setText(string_message);
     }
     public void increment(View view){
-        numberOfCoffees++;
-        display(numberOfCoffees);
+        if(numberOfCoffees<100) {
+            numberOfCoffees++;
+            display(numberOfCoffees);
+        }
+        else{
+            Toast.makeText(MainActivity.this,"Maximum number reached", Toast.LENGTH_LONG).show();
+        }
     }
     public void decrement(View view){
-        numberOfCoffees--;
-        display(numberOfCoffees);
+        if(numberOfCoffees>1) {
+            numberOfCoffees--;
+            display(numberOfCoffees);
+        }
+        else{
+            Toast.makeText(MainActivity.this,"Can't order less than 1", Toast.LENGTH_LONG).show();
+        }
     }
 
 }
